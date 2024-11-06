d9bc8718130153621169658eae898c491768fc2b
gogi <garrybaga4anskiy@gmail.com>
2024-11-07 00:50:24 +0200

    started implementing some logic

--- script.py
+++ script.py
@@ -1,9 +1,19 @@
-import PIL as pl
+from PIL import Image
 import sys
+import random
 
+
+file_id = str(random.randrange(0, 3999, 1))
+if (len(file_id) < 4):
+    file_id = str(((4-len(file_id)))*0) + file_id
+print(file_id)
 file_path = sys.argv[1]
-img = pl.Image.Ope(file_path).convert('L')
-img.save("")
+print(file_path)
+img = Image.open(file_path).convert('L')
+wFile_name = file_path[:file_path.rfind('\\')]
+img.save(wFile_name+file_id+".png")
+
+print(wFile_name+file_id+".png")
 
 
 
