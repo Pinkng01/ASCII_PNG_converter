990a97f0db598fecd3162c2cba7ab40a206200d8
gogi <garrybaga4anskiy@gmail.com>
2024-11-07 20:13:02 +0200

    refactored code and made some exit file name generation improvements

--- script.py
+++ script.py
@@ -2,18 +2,22 @@ from PIL import Image
 import sys
 import random
 
-
-file_id = str(random.randrange(0, 3999, 1))
-if (len(file_id) < 4):
-    file_id = str(((4-len(file_id)))*0) + file_id
-print(file_id)
-file_path = sys.argv[1]
-print(file_path)
-img = Image.open(file_path).convert('L')
-wFile_name = file_path[:file_path.rfind('\\')]
-img.save(wFile_name+file_id+".png")
-
-print(wFile_name+file_id+".png")
-
-
+def PNGname_gen(file_path :str) -> str:
+    file_id = str(random.randrange(0, 3999, 1))
+    if (len(file_id) < 4):
+        file_id = str(((4-len(file_id)))*0) + file_id
+    file_path = file_path[-1:]
+    file_path = file_path[file_path.rfind('\\'):]
+    return file_path+ file_id + ".png"
+if(len(sys.argv) > 1):
+    file_path = sys.argv[1]
+    img = Image.open(file_path).convert('L')
+    wFile_name = PNGname_gen(file_path)
+    if(img.save(wFile_name)):
+        print("sucess!")
+    else:
+        print("failure")
+else:
+    print("pass the path to the image to this script as an argument")
+    print("py script.py path\\to\\file")
 
